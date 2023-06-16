cdc100e1b82f219f1c5363e0a25ff00584c730be
Tina Shi <init.defaultBranch>
2023-06-16 13:57:03 +1000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'spices', to: 'spices#index'
+  post 'spices', to: 'spices#create'
+  patch 'spices/:id', to: 'spices#updated'
+  delete 'spices/:id', to: 'spices#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
